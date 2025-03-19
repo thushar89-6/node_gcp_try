2b9035a0af2e436396ed46f1f6954d2feb2237d8
Ganesh23Nayak <ganeshrnayak23@gmail.com>
2025-03-19 14:28:16 +0530

    firstcommit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('hello bvc');
+});
+
+// Define a route
+app.get('/hello', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
